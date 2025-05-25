f90122cbd9a87a1ebdc77b88a1edb6e3c97e475e
HansHugoHMB <mbayahans@gmail.com>
2025-05-25 21:21:10 +0100

    Create tracker.php

---
+++ tracker.php
@@ -0,0 +1,48 @@
+<?php
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+require 'phpmailer/src/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Lire les données envoyées par JavaScript
+$data = json_decode(file_get_contents("php://input"), true);
+if (!$data) exit('Aucune donnée reçue.');
+
+$message = "
+<b>Nouvelle visite détectée :</b><br><br>
+<strong>IP :</strong> {$data['ip']}<br>
+<strong>Pays :</strong> {$data['country']}<br>
+<strong>Région :</strong> {$data['region']}<br>
+<strong>Ville :</strong> {$data['city']}<br>
+<strong>Fournisseur :</strong> {$data['isp']}<br>
+<strong>Latitude :</strong> {$data['lat']}<br>
+<strong>Longitude :</strong> {$data['lon']}<br>
+<strong>Navigateur :</strong> {$data['ua']}<br>
+<strong>Date :</strong> {$data['time']}
+";
+
+$mail = new PHPMailer(true);
+try {
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'hansmbaya@gmail.com';
+    $mail->Password = 'fwnh etlc yequ suzy'; // mot de passe d'application Gmail
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
+    $mail->setFrom('hansmbaya@gmail.com', 'Tracker Web');
+    $mail->addAddress('mbayahans@gmail.com');
+
+    $mail->isHTML(true);
+    $mail->Subject = 'Nouvelle visite sur ton site';
+    $mail->Body    = $message;
+
+    $mail->send();
+    echo 'OK';
+} catch (Exception $e) {
+    echo "Erreur envoi : {$mail->ErrorInfo}";
+}
+?>
